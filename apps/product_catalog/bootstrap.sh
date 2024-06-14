267806e6f5e7bba00c063ac805d356f2d8bc347c
Edwin Ugwu <edwinbuike@gmail.com>
2024-06-14 05:35:08 +0000

    debugging product_catalog

--- apps/product_catalog/bootstrap.sh
+++ apps/product_catalog/bootstrap.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-export FLASK_APP=./app.py
-export FLASK_DEBUG=1
-flask run -h 0.0.0.0
+#export FLASK_APP=./app.py
+#export FLASK_DEBUG=1
+#flask run -h 0.0.0.0
+
+# Ensure all dependencies are installed
+pip install -r requirements.txt
+
+# Export necessary environment variables
+export FLASK_APP=app.py
+export FLASK_RUN_HOST=0.0.0.0
+export FLASK_RUN_PORT=8080
+
+# Start the Flask application
+flask run
